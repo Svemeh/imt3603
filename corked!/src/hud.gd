0050cd2191a425bd8ac134c52805607c5f5dd3ed
JesperSH <jespersh@stud.ntnu.no>
2026-09-14 10:34:28 +0200

    feat: added functions update_money and update_quota. These update each label when told so

--- corked!/src/hud.gd
+++ corked!/src/hud.gd
@@ -1,15 +1,18 @@
 extends Control
 
-@onready var money_label = $MarginContainer/MoneyLabel
+@onready var money_label = $MarginContainer/VBoxContainer/MoneyLabel
+@onready var quota_label = $MarginContainer/VBoxContainer/QuotaLabel
 
 # Ha to funksjoner - update_money(), update_quota(int: money). Disse har et 
 # tall: starter på null ( 0 ) og oppdateres etter en quota er oppfylt eller en 
 # oppgradering / fiks er kjøpt/betalt for. 
 
-## Updates money icon to X (money)
-func update_money(x: int) -> void:
-	money_label.text = "Money: " + str(x)
+## Updates money icon to amount. It takes the "amount" as an int and updates
+## money label.
+func update_money(amount: int) -> void:
+	money_label.text = "Money: " + str(amount)
 
-## Updates the  quota based on requirements
-func update_quota(money: int):
-	pass
+## Updates the  quota based on requirements. It takes "amount" as an int, while
+## updating the quota label.
+func update_quota(amount: int):
+	quota_label.text = "Quota " + str(amount)
