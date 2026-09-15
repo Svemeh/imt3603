1dbcf398085cd2f2df7607e97ede8b0c0e01eded
EspenG <espegams@ntnu.no>
2026-09-15 12:43:50 +0200

    refactor: Reorganize UI files

--- corked!/src/main_menu.gd
+++ corked!/src/main_menu.gd
@@ -1,16 +1,13 @@
 extends Control
 
-
 func _on_play_button_pressed() -> void:
     get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
 func _on_settings_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/UI/menus/settings_menu.tscn")
-
+    get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
 
 func _on_quit_button_pressed() -> void:
     get_tree().quit()
 
 func _on_credits_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/UI/menus/credits_menu.tscn")
+    get_tree().change_scene_to_file("res://scenes/menus/credits_menu.tscn")
--- corked!/src/settings_menu.gd
+++ corked!/src/settings_menu.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 
 
 func _on_back_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/UI/menus/main_menu.tscn")
+    get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
 
 
 func _on_volume_slider_value_changed(value: float) -> void:
