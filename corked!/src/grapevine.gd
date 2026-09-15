00b0c00b59b7c0c7a9f06bf522cc559e07e39c9f
Svemeh <sverre@maehlum.net>
2026-09-15 14:51:18 +0200

    fix: grapevine now has a cooldown before it can be reharvested

--- corked!/src/grapevine.gd
+++ corked!/src/grapevine.gd
@@ -6,6 +6,7 @@ extends Interactable
 ## straight to the player. Each vine gives one grape.
 
 const GRAPE_SCENE: PackedScene = preload("res://scenes/grape.tscn")
+@onready var timer = $Timer
 
 ## How many grapes a single harvest from this vine yields
 @export var yield_amount: int = 1
@@ -13,9 +14,14 @@ const GRAPE_SCENE: PackedScene = preload("res://scenes/grape.tscn")
 ## Whether this vine has a grape ready to harvest
 var has_grape: bool = true
 
+## Once cooldown timer finishes grape has regrown.
+## Timer is found as a gd Timer node right under the grapevine node
+func _on_my_timer_timeout():
+	has_grape = true
+
 ## Can only be harvested if vine has grape and player hands are free
 func can_interact(player: Player) -> bool:
-	return has_grape and not player.is_holding()
+	return (has_grape and not player.is_holding())
 
 func interact(player: Player) -> void:
 	if not can_interact(player):
@@ -25,7 +31,10 @@ func interact(player: Player) -> void:
 	grape.setup(yield_amount)
 	player.recieve_item(grape)
 	
+	# Bush can not be harvested again before the timer ends
 	has_grape = false
+	timer.start()
+	
 
 func get_prompt() -> String:
 	return "Harvest"
